d56d3918c3e9fa725fb83e834a0d4b5c05146e5d
Steven McVey <smcvey1210@gmail.com>
2022-08-17 21:57:37 -0400

    commit tree 581b7f21ffe83544f1bf7204cf4de0f5715ef74e

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 function addingEventListener() {
 }
+
+
+const input = document.getElementById('button');
+
+function addingEventListener() {
+  input.addEventListener('click', function(){
+    alert('I was clicked');
+});
+}
